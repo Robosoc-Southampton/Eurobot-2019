54fe9aafc5ae3fbb8d258a1d8e4d85d4029964a4
ChazMaWong <48332767+ChazMaWong@users.noreply.github.com>
2019-03-07 20:10:07 +0000

    Update SharpIR.h

--- src/arduino/include/component/SharpIR.h
+++ src/arduino/include/component/SharpIR.h
@@ -8,6 +8,7 @@ public:
 	uint8_t pin;
 	int16_t last_reading;
 	bool enabled;
+	int16_t x_5, x_4, x_3, x_2, x_1, x_0;
 
 	// creates the SharpIR object, with the analogue pin it's connected to
 	SharpIR(uint8_t pin);
