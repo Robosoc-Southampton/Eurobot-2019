c0f3ced66d9a101afea810741b768571b47200fb
exerro <benedict.allen2514@gmail.com>
2019-03-09 17:07:16 +0000

    Fix IR voltage to distance conversion

--- src/arduino/SharpIR.cpp
+++ src/arduino/SharpIR.cpp
@@ -12,15 +12,16 @@ SharpIR::SharpIR(uint8_t pin, int16_t x_0, int16_t x_1, int16_t x_2, int16_t x_3
 }
 
 int16_t SharpIR::read() {
-	last_reading = convertVoltageToDistance(analogRead(pin));
+	// last_reading = convertVoltageToDistance(analogRead(pin));
+	last_reading = analogRead(pin);
 	return last_reading;
 }
 
-int16_t SharpIR::convertVoltageToDistance(int adc_input) {
-	int16_t v_reading = (((int16_t) adc_input) /1023) * 5;
-	int16_t distance = x_0 - (x_1 * v_reading) + (x_2 * pow(v_reading,2)) 
+int16_t SharpIR::convertVoltageToDistance(int16_t adc_input) {
+	float v_reading = (((float) adc_input) / 1023) * 5;
+	float distance = x_0 - (x_1 * v_reading) + (x_2 * pow(v_reading,2)) 
 			- (x_3 * pow(v_reading,3)) + (x_4 * pow(v_reading,4))
 			- (x_5 * pow(v_reading,5));
 	
-	return distance;
+	return (int16_t) distance;
 }
