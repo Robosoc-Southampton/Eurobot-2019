faee44aaedfe8562d97d45b4b5602e1000d2110a
exerro <benedict.allen2514@gmail.com>
2019-03-10 16:46:06 +0000

    Add secondary configuration and raising/lowering of the grabber

--- src/arduino/robot/secondary.c
+++ src/arduino/robot/secondary.c
@@ -1,11 +1,30 @@
 
 #include "include/robot/secondary.h"
 
+#include <Stepper.h>
+
+Stepper raisingStepper(200, 8, 9, 10, 11);
+
+ACTIVITY(raiseStepper, cooldown=1000, count=1000) {
+	raisingStepper.step(5);
+}
+
+ACTIVITY(lowerStepper, cooldown=1000, count=1000) {
+	raisingStepper.step(-5);
+}
+
 int16_t readComponentValue(int16_t component_ID) {
 	return 0u;
 }
 
 struct Activity* lookupActivity(uint16_t activity_ID) {
+	if (activity_ID == 1) {
+		return ACTIVITY(raiseStepper);
+	}
+	if (activity_ID == 2) {
+		return ACTIVITY(lowerStepper);
+	}
+
 	return nullptr;
 }
 
@@ -19,6 +38,9 @@ void setup() {
 	robot::set_distance_sensors(0, sensors);
 
 	robot::setup();
+
+	rlogf("Setting up raising stepper speed");
+	raisingStepper.setSpeed(200);
 }
 
 void loop() {
