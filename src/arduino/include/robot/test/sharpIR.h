56d68b96ac61cd678a8cb7e9270ddbebaf741f9c
exerro <benedict.allen2514@gmail.com>
2019-03-23 18:09:41 +0000

    Improve configurability of SharpIR test

--- src/arduino/include/robot/test/sharpIR.h
+++ src/arduino/include/robot/test/sharpIR.h
@@ -3,8 +3,14 @@
  *     requires connection for meaningful output
  * 
  * Should log "Sensor connected to A0" when connected
- * Should repeatedly log "<reading>" where <reading> is
- *   (the sensor connected to A0)'s reading
+ * Should repeatedly log reading and voltage, depending on config
+ * 
+ * Change config using config-key and config-set messages:
+ *   config-key 0 - sets reading output to false
+ *   config-key 1 - sets reading output to true
+ *   config-key 2 - sets voltage output to false
+ *   config-key 3 - sets voltage output to true
+ *   config-set X - sets delay between outputs to X milliseconds
  */
 
 #pragma once
